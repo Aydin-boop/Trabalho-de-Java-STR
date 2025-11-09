bdb2622237484e2fdc9940b82ce1530b31db849d
Aydin-boop <yavuzincekul@gmail.com>
2025-11-09 15:14:59 +0000

    Add files via upload

--- AxisZ.java
+++ AxisZ.java
@@ -30,12 +30,16 @@ public class AxisZ implements Axis{
                 stop();
             break;
             case 10: //CHECK
-                moveBackward();
-                while (getPos() != 1 && getPos() != 10){}
-                stop();
-                moveForward();
-                while (getPos() != 10){}
-                stop();
+                if (getPos() == 2 || getPos() == 20 || getPos() == 3 || getPos() == 30){
+                    moveBackward();
+                    while (getPos() != 1 && getPos() != 10){}
+                    stop();
+                }
+                else if(getPos() == 1){
+                    moveForward();
+                    while (getPos() != 10){}
+                    stop();
+                }
             break;
 
             case 2: //CHECK
@@ -51,21 +55,29 @@ public class AxisZ implements Axis{
             }
             break;
             case 20: //CHECK
-                moveBackward();
-                while (getPos() != 1 && getPos() != 20){}
-                stop();
-                moveForward();
-                while (getPos() != 20){}
-                stop();
+                if (getPos() == 3 || getPos() == 30){
+                    moveBackward();
+                    while (getPos() != 1 && getPos() != 20){}
+                    stop();
+                }
+                else if(getPos() == 1 || getPos() == 10 || getPos() == 2){
+                    moveForward();
+                    while (getPos() != 20){}
+                    stop();
+                }
             break;
 
             case 3: //CHECK
-                moveBackward();
-                while (getPos() != 1 && getPos() != 3){}
-                stop();
-                moveForward();
-                while (getPos() != 3){}
-                stop();
+                if (getPos() == 30){
+                    moveBackward();
+                    while (getPos() != 1 && getPos() != 3){}
+                    stop();
+                }
+                else if(getPos() == 1 || getPos() == 10 || getPos() == 2 || getPos() == 20){
+                    moveForward();
+                    while (getPos() != 3){}
+                    stop();
+                }
             break;
             case 30: //CHECK
                 moveForward();
